f61f6e5863e05c246ca5a04386dcf8188786146d
Clayton <claytonhart123@yahoo.com>
2019-12-26 12:16:27 -0800

    11

---
+++ 11.js
@@ -0,0 +1,19 @@
+var maxArea = function(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+    
+    while (left < right) {
+        let y = Math.min(height[left], height[right]);
+        let area = y * (right - left);
+        max = Math.max(max, area);
+        
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return max;
+};
