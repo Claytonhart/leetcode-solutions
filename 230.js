5042f997baef2f7a6568337b32b70bc82343475b
Clayton <claytonhart123@yahoo.com>
2019-12-26 09:08:03 -0800

    230

---
+++ 230.js
@@ -0,0 +1,14 @@
+function kthSmallest(root,k) {
+    let stack = [];
+    
+    while (true) {
+        while (root !== null) {
+            stack.push(root);
+            root = root.left
+        }
+        root = stack.pop();
+        k--;
+        if (k === 0) return root.val;
+        root = root.right;
+    }
+}
