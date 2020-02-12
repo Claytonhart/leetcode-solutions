95d1531769edca8f6e3279b33d2a11043cee7a47
Clayton Hart <claytonhart123@yahoo.com>
2020-02-12 09:38:14 -0800

    70

---
+++ 70. Climbing Stairs.js
@@ -0,0 +1,12 @@
+var climbStairs = function(n) {
+  let prev = 1;
+  let current = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let next = prev + current;
+    prev = current;
+    current = next;
+  }
+
+  return current;
+};
