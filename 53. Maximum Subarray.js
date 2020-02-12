409cac44e00a4af48f7d9900658e70a6655fc2bf
Clayton Hart <claytonhart123@yahoo.com>
2020-02-12 07:00:22 -0800

    53

---
+++ 53. Maximum Subarray.js
@@ -0,0 +1,11 @@
+var maxSubArray = function(nums) {
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum = Math.max(sum + nums[i], nums[i]);
+    max = Math.max(max, sum);
+  }
+
+  return max;
+};
